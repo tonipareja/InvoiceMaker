e02c261b9ace5eb3d2150e29f720ccbd95d50df4
Toni Pareja <67589615+tonipareja@users.noreply.github.com>
2021-02-11 09:01:32 +0100

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <Navbar />
       <section class="max-w-4xl mx-auto text-center mt-16 sm:mt-32">
         <h1 class="text-4xl sm:text-6xl max-w-3xl font-bold mx-auto mb-4">Crea facturas y envíalas a tus clientes en segundos</h1>
-        <p class="text-base sm:text-xl mx-auto mb-8">Crea facturas de manera gratuita y mándaselas directaemente a tu cliente desde nuestra plataforma. Ahorra en gestorías y ahorra en tiempo.</p>
+        <p class="text-base sm:text-xl mx-auto mb-8">Crrea facturas de manera gratuita y mándaselas directaemente a tu cliente desde nuestra plataforma. Ahorra en gestorías y ahorra en tiempo.</p>
         <div class="flex flex-wrap items-center justify-center px-8">
           <button class="text-base sm:text-xl py-3 px-8 border-2 border-green-500 rounded-lg bg-green-500 hover:bg-green-700 hover:border-green-700 text-gray-50 w-full sm:w-auto mb-4 sm:mb-0 sm:mr-4 focus:outline-none">Crear Factura</button>
           <button class="text-base sm:text-xl py-3 px-8 border-2 border-green-500 rounded-lg text-green-500 hover:bg-green-700 hover:border-green-700 hover:text-gray-50 w-full sm:w-auto focus:outline-none">Go to Dashboard</button>
