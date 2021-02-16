61ffe8eb55c8487fc24ee934244872efb6180c73
Toni Pareja <67589615+tonipareja@users.noreply.github.com>
2021-02-16 22:13:56 +0100

    Firebase installed

---
+++ components/auth/SocialLoginButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="text-base sm:text-xl py-3 px-8 border-2 rounded-lg text-grisaceo w-full sm:w-auto mb-4 focus:outline-none"
+    :class="type"
+    @click="$emit('click')"
+  >
+    <i class="fab fa-google mr-4"></i>Iniciar sesión con Google
+  </button>
+</template>
+
+<script>
+export default {
+  name: "SocialLoginButton",
+  props: {
+    type: {
+      type: String,
+      default: "google",
+      validate: value => ["google"].includes(value)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.google {
+  background-color: #db4a39;
+  border-color: #db4a39;
+}
+
+.google:hover {
+  background-color: #bb3222;
+  border-color: #bb3222;
+}
+</style>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="container max-w-5xl mx-auto px-4 sm:px-6">
     <section class="max-w-4xl mx-auto text-center mt-16 sm:mt-32">
-      <h1 class="text-3xl font-bold ">Log In page</h1>
-      <p>Iniciar sesión con Google</p>
-
+      <h1 class="text-3xl font-bold mb-8">Log In</h1>
+      <SocialLoginButton type="google" @click="loginWithGoogle" />
     </section>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    async loginWithGoogle() {
+      try {
+        const provider = new this.$fireAuthObj.GoogleAuthProvider();
+        const result = await this.$fireAuth.signInWithPopup(provider);
+        console.log(result);
+      } catch {
+        //TODO show toast
+        console.log("login error");
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
